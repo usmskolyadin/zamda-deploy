753d6d546a542330fc429f356cd72f1cb193b015
usmskolyadin <koladinmaksim246@gmail.com>
2026-05-15 23:50:06 +0300

    fix header

--- frontend/src/widgets/search-bar/index.tsx
+++ frontend/src/widgets/search-bar/index.tsx
@@ -72,7 +72,7 @@ export default function SearchBar() {
       <input
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="p-4 border-0.5  border text-gray-900 border-black rounded-3xl ml-2 lg:h-[44px] h-[35px] lg:w-[510px] w-65"
+        className="p-4 border-0.5  border text-gray-900 border-black rounded-3xl ml-2 lg:h-[44px] h-[35px] lg:w-[510px] w-60"
         placeholder="Find an Ad..."
         type="text"
       />
